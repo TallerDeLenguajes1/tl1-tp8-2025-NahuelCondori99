3574472be352007be2a31db370d13541c7d0e134
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-03 14:48:56 -0300

    Creo mi archivo Todo y cree mi class Tarea

---
+++ ToDo/Tarea.cs
@@ -0,0 +1,28 @@
+namespace Tareas;
+
+public class Tarea 
+{
+    private int tareaId;
+    private string descripcion;
+    private int duracion;
+
+    public Tarea(int tareaId, string descripcion, int duracion)
+    {
+        this.tareaId = tareaId;
+        this.descripcion = descripcion;
+        this.duracion = duracion;
+    }
+
+    public int TareaId { get => tareaId; set => tareaId = value; }
+    public string Descripcion { get => descripcion; set => descripcion = value; }
+    public int Duracion { get => duracion; 
+        set{
+        if(value >10 || value <100)
+        {
+            duracion = value;
+        }
+    } }
+
+    
+    /*Validar que esté entre 10 y 100 Puedes añadir un constructor y métodos auxiliares si lo consideras necesario*/
+}
