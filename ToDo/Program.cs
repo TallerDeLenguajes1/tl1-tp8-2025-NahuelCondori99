76617a9f43993ab35ca731b164ba64a1cb87a180
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-03 16:10:29 -0300

    Revisando orque no funciona

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -4,10 +4,10 @@
 
 Tarea[] misTareas = new Tarea[3];
 
-
-    misTareas[0]= new Tarea(1, "Lavar la fruta", 12);
-    misTareas[1]= new Tarea(2, "Lavar", 20);
-    misTareas[2]= new Tarea(3, "programar", 10);
+//No hace falta hacer el arreglo de tareas, puedo hacerlo directamente con la lista
+    misTareas[0]= new Tarea(1, "Programar en C#", 12);
+    misTareas[1]= new Tarea(2, "Llorar porque no me sale", 20);
+    misTareas[2]= new Tarea(3, "Reirme de la situacion", 10);
     /*Console.WriteLine("Ingresar Descripcion:");
     descrip = Console.ReadLine();
     Console.WriteLine("Ingresar duracion:");
@@ -26,10 +26,33 @@ for (int i = 0; i < 3; i++)
 
 }
 
-foreach (var item in TareasPendientes)
+Console.WriteLine("Ingrese la ID de la tarea que ya realizo: ");
+string idMov = Console.ReadLine();
+int idMover = int.Parse(idMov);
+
+foreach (Tarea tareaBus in TareasPendientes)
+{
+    if (idMover == tareaBus.TareaId)
+    {
+        TareasRealizadas.Add(tareaBus);
+        TareasPendientes.Remove(tareaBus);
+    }
+}
+
+foreach (var TareaP in TareasPendientes)
+{
+    Console.WriteLine("Tarea ID: " + TareaP.TareaId);
+    Console.WriteLine("Descripcion: " + TareaP.Descripcion);
+    Console.WriteLine("Duracion: " + TareaP.Duracion + " minutos");
+}
+
+Console.WriteLine("-------------------------");
+
+/*
+foreach (var item in TareasRealizadas)
 {
     Console.WriteLine("Tarea ID: " + item.TareaId);
     Console.WriteLine("Descripcion: " + item.Descripcion);
     Console.WriteLine("Duracion: " + item.Duracion);
 }
-
+*/
