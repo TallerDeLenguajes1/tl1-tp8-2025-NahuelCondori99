cdfc5cfc3dd4881b7ca0f05a5366abcbca32565c
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-08 23:41:25 -0300

    Realice mi funcion para buscar tareas por descripcion

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -2,44 +2,46 @@
 
 //Cargo mis datos
 
-Tarea[] misTareas = new Tarea[3];
+//Tarea[] misTareas = new Tarea[3];
 
 //No hace falta hacer el arreglo de tareas, puedo hacerlo directamente con la lista
-    misTareas[0]= new Tarea(1, "Programar en C#", 12);
-    misTareas[1]= new Tarea(2, "Llorar porque no me sale", 20);
-    misTareas[2]= new Tarea(3, "Reirme de la situacion", 10);
-    /*Console.WriteLine("Ingresar Descripcion:");
-    descrip = Console.ReadLine();
-    Console.WriteLine("Ingresar duracion:");
-    dura = int.Parse(Console.ReadLine());
-    id = i+1;
-    misTareas[i] = new Tarea(tareaId, descripcion, duracion); */
-
+    //misTareas[0]= new Tarea(1, "Programar en C#", 12);
+    //misTareas[1]= new Tarea(2, "Llorar porque no me sale", 20);
+    //misTareas[2]= new Tarea(3, "Reirme de la situacion", 10);
 
 //Creo mis listas
-List<Tarea> TareasPendientes = new List<Tarea>();
+List<Tarea> TareasPendientes = new List<Tarea>
+{
+    new Tarea(1, "Programar en C#", 12),
+    new Tarea(2, "Llorar porque no me sale", 20),
+    new Tarea(3, "Reirme de la situacion", 10),
+    new Tarea(4, "Realizar la buena practica en C#", 30)
+};
+
+
 List<Tarea> TareasRealizadas = new List<Tarea>();
 
-for (int i = 0; i < 3; i++)
+/*for (int i = 0; i < 3; i++)
 {
     TareasPendientes.Add(misTareas[i]);
 
-}
+}*/
 
 Console.WriteLine("Ingrese la ID de la tarea que ya realizo: ");
 string idMov = Console.ReadLine();
 int idMover = int.Parse(idMov);
 
-foreach (Tarea tareaBus in TareasPendientes)//Usar un for no el foreach
+for (int i = 0; i < 3; i++)//Usar un for no el foreach
 {
-    if (idMover == tareaBus.TareaId)
+    if (idMover == TareasPendientes[i].TareaId)
     {
-        TareasRealizadas.Add(tareaBus);
-        TareasPendientes.Remove(tareaBus);//Usar removeat
+        TareasRealizadas.Add(TareasPendientes[i]);
+        TareasPendientes.RemoveAt(i);
+        break;
     }
 }
 
-foreach (var TareaP in TareasPendientes)
+foreach (Tarea TareaP in TareasPendientes)
 {
     Console.WriteLine("Tarea ID: " + TareaP.TareaId);
     Console.WriteLine("Descripcion: " + TareaP.Descripcion);
@@ -48,11 +50,34 @@ foreach (var TareaP in TareasPendientes)
 
 Console.WriteLine("-------------------------");
 
-/*
-foreach (var item in TareasRealizadas)
+
+foreach (Tarea TareaR in TareasRealizadas)
 {
-    Console.WriteLine("Tarea ID: " + item.TareaId);
-    Console.WriteLine("Descripcion: " + item.Descripcion);
-    Console.WriteLine("Duracion: " + item.Duracion);
+    Console.WriteLine("Tarea ID: " + TareaR.TareaId);
+    Console.WriteLine("Descripcion: " + TareaR.Descripcion);
+    Console.WriteLine("Duracion: " + TareaR.Duracion + " minutos");
 }
-*/
+
+Console.WriteLine("-------------------------");
+
+static void BuscarxDescripcion(List<Tarea> TareasPendientes)
+{
+    Console.WriteLine("Ingrese descripcion de la tarea pendiente para iniciar la busqueda:");
+    string descripB = Console.ReadLine();
+    bool encontrado = false;
+
+    foreach (Tarea descrip in TareasPendientes)
+    {
+        if (descrip.Descripcion.Contains(descripB, StringComparison.OrdinalIgnoreCase) && descrip.Descripcion != null)
+        {
+            descrip.MostrarTareas();
+            encontrado = true;
+        }
+    }
+    if (!encontrado)
+    {
+        Console.WriteLine("No hay tareas pendientes con esa descripcion.");
+    }
+}
+
+BuscarxDescripcion(TareasPendientes);
